2c4a45a0ee42ae22baaae8a543009b070eef8cd8
Meghna Srivastava <meghna.s1906@gmail.com>
2018-04-19 16:49:22 +0200

    making a post clickable

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -8,7 +8,8 @@ import './Blog.css';
 
 class Blog extends Component {
     state = {
-        posts: []
+        posts: [],
+        selectedPostId: null
     };
 
     componentDidMount() {
@@ -25,11 +26,20 @@ class Blog extends Component {
                 this.setState({ posts: updatedPosts })
                 // console.log(response);
             });
+    }
+
+    postSelectedHandler = (id) => {
+        this.setState({selectedPostId: id});
 
     }
+
     render() {
         const posts = this.state.posts.map(post => {
-            return <Post key={post.id} title={post.title} author={post.author} />;
+            return <Post
+                key={post.id}
+                title={post.title}
+                author={post.author}
+                clicked={() => this.postSelectedHandler(post.id)} />;
         })
 
         return (
@@ -38,7 +48,7 @@ class Blog extends Component {
                     {posts}
                 </section>
                 <section>
-                    <FullPost />
+                    <FullPost id={this.state.selectedPostId} />
                 </section>
                 <section>
                     <NewPost />
