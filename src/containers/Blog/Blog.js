af790517fbedaed1c827e74f650fc02c9d28521b
Meghna Srivastava <meghna.s1906@gmail.com>
2018-04-20 09:47:21 +0200

    setting a default global configuration for axios

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -14,7 +14,7 @@ class Blog extends Component {
     };
 
     componentDidMount() {
-        axios.get('https://jsonplaceholder.typicode.com/posts')
+        axios.get('/posts')
             .then(response => {
                 const posts = response.data.slice(0, 4); //only fetch 4 posts
                 const updatedPosts = posts.map(post => {
--- src/index.js
+++ src/index.js
@@ -23,5 +23,10 @@ axios.interceptors.response.use(response => {
     return Promise.reject(error)
 });
 
+// Setting a default global configuration for axios:
+axios.defaults.baseURL = 'https://jsonplaceholder.typicode.com'
+axios.defaults.headers.common['Authorisation'] = 'AUTH TOKEN';
+axios.defaults.headers.post['Content-Type'] = 'application/json';
+
 ReactDOM.render(<App />, document.getElementById('root'));
 
