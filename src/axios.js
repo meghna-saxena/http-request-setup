3407589557979f6da951c76a4ce867b42827828f
Meghna Srivastava <meghna.s1906@gmail.com>
2018-04-20 10:40:01 +0200

    creating and using axios instances

--- src/axios.js
+++ src/axios.js
@@ -1,10 +1,19 @@
 import axios from 'axios';
 
-const instance = axios.create({
-    baseURL: 'https://jsonplaceholder.typicode.com'
-});
+// const instance = axios.create({
+//     baseURL: 'https://jsonplaceholder.typicode.com'
+// });
 
-instance.defaults.headers.common['Authorisation'] = 'AUTH TOKEN FROM INSTANCE'; //overrides the defaults authorisation
+//overrides the defaults authorisation
+// instance.defaults.headers.common['Authorisation'] = 'AUTH TOKEN FROM INSTANCE'; 
+
+// Another way of writing
+const instance = axios.create({
+    baseURL: 'https://jsonplaceholder.typicode.com',
+    headers: {
+    'Authorization' : 'AUTH TOKEN FROM INSTANCE'
+    }
+    });
 
 //instance.interceptors.request...
 
