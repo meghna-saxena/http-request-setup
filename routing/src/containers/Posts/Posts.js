caf1db07d284ac409c31ea7521b28b63391c92ce
Meghna Srivastava <meghna.s1906@gmail.com>
2018-04-21 15:07:31 +0200

    refactored components into containers

---
+++ routing/src/containers/Posts/Posts.js
@@ -0,0 +1,55 @@
+import React, { Component } from 'react';
+import axios from '../../axios';
+import Post from '../../components/Post/Post';
+import './Posts.css';
+
+class Posts extends Component {
+    state = {
+        posts: []
+    };
+
+    componentDidMount() {
+        axios.get('/posts')
+            .then(response => {
+                const posts = response.data.slice(0, 4); //only fetch 4 posts
+                const updatedPosts = posts.map(post => {
+                    return {
+                        ...post, //distribute the properties of the posts we got from the server
+                        author: 'Meggie' //added another property
+                    }
+                });
+                // this.setState({ posts: response.data });//data property has an array of posts
+                this.setState({ posts: updatedPosts })
+                // console.log(response);
+            })
+            .catch(error => {
+                console.log(error);
+                // this.setState({ error: true });
+            });
+    }
+
+    postSelectedHandler = (id) => {
+        this.setState({ selectedPostId: id });
+    }
+
+    render() {
+        let posts = <p style={{ textAlign: 'center' }}>Something went wrong!</p>
+        if (!this.state.error) {
+            posts = this.state.posts.map(post => {
+                return <Post
+                    key={post.id}
+                    title={post.title}
+                    author={post.author}
+                    clicked={() => this.postSelectedHandler(post.id)} />
+            });
+        }
+
+        return (
+            <section className="Posts">
+                {posts}
+            </section>
+        );
+    }
+}
+
+export default Posts;
